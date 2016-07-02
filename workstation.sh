8cf892ca35e7d0c1b876dce39f9f49a4310d870d
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-07-02 13:58:02 -0500

    Remove explicit dependencies

--- workstation.sh
+++ workstation.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash -e
 
-brew install autoconf heroku openssl pkg-config postgresql rbenv readline ruby-build ruby-install
+brew install heroku postgresql rbenv ruby-build
 brew cask install 1password flycut google-chrome iterm2 java rubymine screenhero shiftit sublime-text syncthing-bar
