9521d85816962fe5a57a471606febec6e565e5c6
Leonhardt Koepsell <leonhardt@koepsell.io>
2016-09-09 09:32:59 -0700

    Add node

--- workstation.sh
+++ workstation.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash -e
 
-brew install heroku postgresql rbenv ruby-build
+brew install heroku postgresql rbenv ruby-build node
 brew cask install 1password flycut google-chrome intellij-idea iterm2 java rubymine screenhero shiftit sublime-text syncthing-bar
