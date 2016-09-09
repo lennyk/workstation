bf36b8d558ad5e8bca70896f5be758ee5678219f
Leonhardt Koepsell <leonhardt@koepsell.io>
2016-09-09 14:14:30 -0700

    Add git configuration

--- workstation.sh
+++ workstation.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash -e
 
 brew install heroku postgresql rbenv ruby-build node
-brew cask install 1password flycut google-chrome intellij-idea iterm2 java rubymine screenhero shiftit sublime-text syncthing-bar
+brew cask install 1password flycut google-chrome intellij-idea iterm2 java rubymine screenhero shiftit sublime-text syncthing-bar git
+
+git config --global push.default simple
+git config --global user.name "Leonhardt Koepsell"
+git config --global user.email "leonhardt@koepsell.io"
