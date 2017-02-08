077c4cfc42d43e62c7b817300845461d066c25d2
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-02-08 15:51:04 -0800

    Fix homebrew commands

--- workstation.sh
+++ workstation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -e
 
-brew install heroku postgresql rbenv ruby-build node
-brew cask install 1password flycut google-chrome intellij-idea iterm2 java rubymine screenhero shiftit sublime-text syncthing-bar git
+brew install postgresql rbenv ruby-build node git
+brew cask install 1password flycut google-chrome intellij-idea iterm2 java rubymine screenhero shiftit sublime-text syncthing-bar
 
 git config --global push.default simple
 git config --global user.name "Leonhardt Koepsell"
