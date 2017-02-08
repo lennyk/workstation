c2cb98d189369e50945ff28ca20db6c9983c2bab
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-02-08 15:26:01 -0800

    Add .ssh directory setup

--- workstation.sh
+++ workstation.sh
@@ -6,3 +6,8 @@ brew cask install 1password flycut google-chrome intellij-idea iterm2 java rubym
 git config --global push.default simple
 git config --global user.name "Leonhardt Koepsell"
 git config --global user.email "leonhardt@koepsell.io"
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/authorized_keys
