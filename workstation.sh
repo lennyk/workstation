08362c6235385c9a2eb80d41493852b2db3b4be9
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-07-10 04:22:24 -0500

    Automatically start Homebrew services

--- workstation.sh
+++ workstation.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash -e
 
 brew install git yarn gradle tree
+
 brew install mysql mailhog rabbitmq
+brew services start mysql
+brew services start mailhog
+brew services start rabbitmq
+
 brew cask install 1password flycut google-chrome intellij-idea iterm2 java shiftit syncthing-bar cyberduck tunnelblick visual-studio-code docker
 
 git config --global push.default simple
